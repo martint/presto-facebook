d9ed00f5eabc1b6e62e16b41bee15562ecf42ee3
Dain Sundstrom <dain@iq80.com>
2013-12-05 17:41:37 -0800

    Remove getTypes, getFieldCount, and getFieldOffset from TupleInfo

--- presto-main/src/main/java/com/facebook/presto/sql/planner/LocalExecutionPlanner.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/LocalExecutionPlanner.java
@@ -615,7 +615,7 @@ public class LocalExecutionPlanner
         {
             Builder<Input, Type> inputTypes = ImmutableMap.builder();
             for (Input input : ImmutableSet.copyOf(layout.values())) {
-                TupleInfo.Type type = tupleInfos.get(input.getChannel()).getTypes().get(input.getField());
+                TupleInfo.Type type = tupleInfos.get(input.getChannel()).getType();
                 switch (type) {
                     case BOOLEAN:
                         inputTypes.put(input, Type.BOOLEAN);
