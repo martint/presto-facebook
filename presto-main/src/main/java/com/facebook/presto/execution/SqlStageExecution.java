2bcac86245abf74d31555c0d01fc2856138c6c96
Dain Sundstrom <dain@iq80.com>
2013-12-05 17:41:40 -0800

    Log query timeline

--- presto-main/src/main/java/com/facebook/presto/execution/SqlStageExecution.java
+++ presto-main/src/main/java/com/facebook/presto/execution/SqlStageExecution.java
@@ -52,6 +52,7 @@ import io.airlift.log.Logger;
 import io.airlift.stats.Distribution;
 import io.airlift.units.DataSize;
 import io.airlift.units.Duration;
+import org.joda.time.DateTime;
 
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.GuardedBy;
@@ -130,7 +131,10 @@ public class SqlStageExecution
     @GuardedBy("this")
     private OutputBuffers subStageOutputBuffers = INITIAL_EMPTY_OUTPUT_BUFFERS;
 
+
     private final ExecutorService executor;
+
+    private final AtomicReference<DateTime> schedulingComplete = new AtomicReference<>();
 
     private final Distribution getSplitDistribution = new Distribution();
     private final Distribution scheduleTaskDistribution = new Distribution();
@@ -331,6 +335,7 @@ public class SqlStageExecution
             }
 
             StageStats stageStats = new StageStats(
+                    schedulingComplete.get(),
                     getSplitDistribution.snapshot(),
                     scheduleTaskDistribution.snapshot(),
                     addSplitDistribution.snapshot(),
@@ -504,6 +509,7 @@ public class SqlStageExecution
                     throw new IllegalStateException("Unsupported partitioning: " + fragment.getPartitioning());
                 }
 
+                schedulingComplete.set(DateTime.now());
                 stageState.set(StageState.SCHEDULED);
 
                 // add the missing exchanges output buffers
